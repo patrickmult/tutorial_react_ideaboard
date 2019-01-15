7a462def83dfd2a7b7d21547ef759d953ec38aaa
Patrick Multhaup <pjmulthaup@gmail.com>
2019-01-14 20:12:09 -0800

    ideas update done

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import IdeasContainer from './components/IdeasContainer'
+import IdeasContainer from './components/ideas/IdeasContainer'
 
-class App extends Component {
-  render() {
-    return (
+function App(props) {
+  return(
       <div className="App">
         <div className="App-header">
           <h1>Idea Board</h1>
         </div>
         <IdeasContainer />
       </div>
-    );
-  }
+  );
 }
 
 export default App;
